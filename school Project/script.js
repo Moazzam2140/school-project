ae56cf8eeabad5756f6dcf89ad29c2e853bf6618
Moazzam2140 <159418905+Moazzam2140@users.noreply.github.com>
2024-02-08 13:34:13 +0530

    Add files via upload

---
+++ school Project/script.js
@@ -0,0 +1,10 @@
+let login = document.querySelector("#login");
+login.addEventListener("click", function(){
+   this.value("login");
+})
+
+let home=document.querySelector("#home");
+home.addEventListener("click", function(){
+   this.value("home");
+})
+ 
